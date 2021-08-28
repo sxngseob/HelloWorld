0aebeb3147dd048a13a70ffc480854aee832a20d
brianwilkinson <brian.wilkinson@ssfs.org>
2021-08-28 08:25:13 -0400

    Initial commit

---
+++ Tests/HelloTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test
+    void mainTest() {
+       // Hello.main(null);
+        assertTrue(outContent.toString().contains("Hello, World!"), "Check spelling, spacing, and punctuation.");
+
+    }
+}
